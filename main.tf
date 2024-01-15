717122e93e128f329d0750992f83e41273eda3fe
Festus Wilhelm Henkel <belos117@gmail.com>
2024-01-15 05:40:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,37 @@ resource "aws_instance" "myliveserver" {
     Name = "dev"
   }
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+ 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["116.30.20.50/32"]
+ 
+  }
+
+  ingress {
+   description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["116.30.20.50/32"]
+ }
+
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+   cidr_blocks      = ["116.30.20.50/32"]
+  }
+
+  tags = {
+    Name = “manual”
+  } 
+}
