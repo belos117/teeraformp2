bdc5e8684927772078a3e2103fac6e91b48230f0
Festus Wilhelm Henkel <belos117@gmail.com>
2024-01-16 12:44:19 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -15,3 +15,28 @@ variable "environment" {
   type        = string
   default     = "dev" 
 }
+
+variable = "https_ingress" {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["116.30.20.50/32"]
+ 
+  }
+
+  variable = "http_ingress" {
+   description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["116.30.20.50/32"]
+ }
+
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+   cidr_blocks      = ["116.30.20.50/32"]
+  }
